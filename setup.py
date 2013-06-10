29d751a015951a5be90deeb6f1f46cfcf0158684
Theo Chatzimichos <tampakrap@gentoo.org>
2013-06-11 00:40:22 +0200

    Updates in setup.py, version bump

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+import os
 
 setup(
     name="edpwd",
-    version="0.0.1",
+    version="0.0.2",
     description="Encrypt/Decrypt password functions that wrap up Blowfish",
     long_description=open('README.md').read(),
-    url="http://github.com/tampakrap/edpwd/",
+    url="http://github.com/tampakrap/edpwd",
     author="Theo Chatzimichos",
     author_email="tampakrap@gmail.com",
     license="BSD",
-    packages=["edpwd"],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
     keywords=["password", "hash", "encryption", "decryption"],
+    packages=["edpwd"],
+    data_files=[('', ['LICENSE', 'README.md'])],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
     install_requires=[
         "pycrypto>=2.6",
     ]
