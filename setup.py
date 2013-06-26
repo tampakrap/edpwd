e7fed237ec1cf07686462c0d1dac6098bed3046e
Theo Chatzimichos <tampakrap@gentoo.org>
2013-06-26 23:56:51 +0200

    New version 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="edpwd",
-    version="0.0.4",
+    version="0.0.5",
     description="Encrypt/Decrypt password functions that wrap up Blowfish",
     long_description=open('README.md').read(),
     url="http://github.com/tampakrap/edpwd",
